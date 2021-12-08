a8fc59e47349aa06af788a62855979ae0a1838c9
VladGusev <vlad-gusev-official@mail.ru>
2021-12-08 11:21:09 +0300

    5pr

---
+++ src/ru/mirea/task11/Num1/TestGame.java
@@ -0,0 +1,7 @@
+package ru.mirea.task11.Num1;
+
+public class TestGame {
+    public static void main(String[] argv) {
+        (new Game()).setVisible(true);
+    }
+}
---
+++ src/ru/mirea/task11/Num2/Test.java
@@ -0,0 +1,7 @@
+package ru.mirea.task11.Num2;
+
+public class Test {
+    public static void main(String[] args) {
+        (new MouseEventApp()).setVisible(true);
+    }
+}
